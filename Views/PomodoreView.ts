a894fb586e607a5a16adb17588c4fe19fbe2ff44
Maike <maike_jbe@hotmail.com>
2018-04-21 18:49:54 -0300

    Adicionada estilização

--- Views/PomodoreView.ts
+++ Views/PomodoreView.ts
@@ -14,9 +14,13 @@ export class PomodoreView implements Observer {
     public update(title: string): this {
         this.timerTitle.innerHTML = title;
         if (title === Mode.SESSION) {
+            this.timerTitle.classList.add("title-session")
+            this.timerTitle.classList.remove("title-break")
             this.display.classList.add("display-session")
             this.display.classList.remove("display-break")
         } else {
+            this.timerTitle.classList.add("title-break")
+            this.timerTitle.classList.remove("title-session")
             this.display.classList.add("display-break")
             this.display.classList.remove("display-session")
         }
