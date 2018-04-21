58ef6c8c5c3346a421a11b5d27877d1149e2e8aa
Maike <maike_jbe@hotmail.com>
2018-04-21 17:32:19 -0300

    código do arquivo main movido para o controlador

--- Controllers/PomodoreController.ts
+++ Controllers/PomodoreController.ts
@@ -1,27 +1,34 @@
+import {TimerView} from "../Views/TimerView"
+import {PomodoreView} from "../Views/PomodoreView"
+import {Timer} from "../Models/Timer"
 import {Pomodore} from "../Models/Pomodore"
 
-export class PomodoreController {
-    private inputBreak: HTMLInputElement;
-    private inputSession: HTMLInputElement;
-    private pomodore: Pomodore;
+export class PomodoreController
+{
+    public init()
+    {
+        let display = <HTMLElement>document.querySelector("#timer")
+        let clock = <HTMLElement>document.querySelector("#timer p")
+        let timerTitle = <HTMLElement>document.querySelector(".timer-type")
+        let button = <HTMLButtonElement>document.querySelector(".bt-apply")
 
-    public constructor (
-        inputBreak:HTMLInputElement,
-        inputSession:HTMLInputElement,
-        pomodore: Pomodore
-    ) {
-        this.inputBreak = inputBreak;
-        this.inputSession = inputSession;
-        this.pomodore = pomodore
-    }
+        let timerView = new TimerView(display, clock, button)
+        let pomodoreView = new PomodoreView(timerTitle, display)
 
-    public applyTimeValues(): this {
-        this.pomodore.setBreak(parseInt(this.inputBreak.value));
-        this.pomodore.setSession(parseInt(this.inputSession.value));
-        this.pomodore.timer.notify({
-            minutes: this.inputSession.value,
-            seconds: 60
-        });
-        return this;
+        let timerSession = new Timer
+        let timerBreak = new Timer
+
+        let pomodore = new Pomodore(timerSession, timerBreak, timerView)
+
+        pomodore.attach(pomodoreView)
+        timerSession.attach(timerView)
+        timerBreak.attach(timerView)
+
+        let inputBreak = <HTMLInputElement>document.querySelector('.break-time')
+        let inputSession = <HTMLInputElement>document.querySelector('.session-time')
+
+        timerSession.setTime(parseInt(inputSession.value))
+        timerBreak.setTime(parseInt(inputBreak.value))
+        pomodore.play();
     }
 }
